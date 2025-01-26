06bae2787183bac59984af6f1de210bb626c46c8
rejishal <rejishal@gmail.com>
2025-01-26 11:49:53 +0530

    Removed DVC

--- Model/DataPreprocessing.py
+++ Model/DataPreprocessing.py
@@ -1,9 +1,10 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-def load_and_preprocess_data(output_file="data/preprocessed_data/processed_data.csv"):
-    data_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.cleveland.data"
-    dataset = pd.read_csv(data_url, header=None, na_values="?")
+def load_and_preprocess_data(input_file="data/raw_data/raw_data.csv", output_file="data/preprocessed_data/processed_data.csv"):
+    #data_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.cleveland.data"
+    #dataset = pd.read_csv(input_file, header=None, na_values="?")
+    dataset = pd.read_csv(input_file)
     dataset.columns = [
         "age", "sex", "cp", "trestbps", "chol", "fbs", "restecg", "thalach", "exang", "oldpeak", "slope", "ca", "thal", "target"
     ]
